d4f4cfb3a8c80327d2307c2c7360fa20b4580d25
Luis Nunez <luisnunez91@gmail.com>
2026-04-04 11:29:03 -0600

    editar nombres en la tabla

--- bandnames/src/components/BandList.js
+++ bandnames/src/components/BandList.js
@@ -7,6 +7,21 @@ export const BandList = ({ bands }) => {
     setBandas(bands);
   }, [bands]);
 
+  const cambioNombre = (event, id) => {
+    setBandas(
+      bandas.map((band) => {
+        if (band.id === id) {
+          band.name = event.target.value;
+        }
+        return band;
+      }),
+    );
+  };
+
+  const onPerdioFoco = (id, nombre) => {
+    console.log(id, nombre);
+  };
+
   const createRows = () => {
     return bandas.map((band) => (
       <tr key={band.id}>
@@ -14,10 +29,15 @@ export const BandList = ({ bands }) => {
           <button className="btn btn-primary">+1</button>
         </td>
         <td>
-          <input value={band.name} className="form-control" />
+          <input
+            value={band.name}
+            className="form-control"
+            onChange={(event) => cambioNombre(event, band.id)}
+            onBlur={() => onPerdioFoco(band.id, band.name)}
+          />
         </td>
         <td>
-          <h3>10</h3>
+          <h3>{band.votes}</h3>
         </td>
         <td>
           <button className="btn btn-danger">Borrar</button>
