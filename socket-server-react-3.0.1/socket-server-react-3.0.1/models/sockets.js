686820b7cfd91fe9f1eb2561303ad24fcf4c7ab8
Luis Nunez <luisnunez91@gmail.com>
2026-04-04 08:59:53 -0600

    agregar evento de sockets

--- socket-server-react-3.0.1/socket-server-react-3.0.1/models/sockets.js
+++ socket-server-react-3.0.1/socket-server-react-3.0.1/models/sockets.js
@@ -1,10 +1,12 @@
-
+const BandList = require('./band-list');
 
 class Sockets {
 
     constructor( io ) {
 
         this.io = io;
+
+        this.bandList = new BandList();
 
         this.socketEvents();
     }
@@ -13,12 +15,9 @@ class Sockets {
         // On connection
         this.io.on('connection', ( socket ) => {
 
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
+            console.log('Cliente conectado', socket.id);
+
+            socket.emit('current-bands', this.bandList.getBands());
             
         
         });
