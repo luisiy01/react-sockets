5e4cbb4352125e3b6ca03c309e3f36e45a90b1a2
Luis Nunez <luisnunez91@gmail.com>
2026-04-06 11:32:29 -0600

    agregar router

--- ticket-app/src/pages/RouterPage.js
+++ ticket-app/src/pages/RouterPage.js
@@ -1,55 +1,49 @@
-import React from "react";
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
+import { UserOutlined, VideoCameraOutlined } from "@ant-design/icons";
 import { Layout, Menu } from "antd";
+import { Routes, Route, Link, BrowserRouter as Router } from "react-router";
+import Ingresar from "./Ingresar";
+import Cola from "./Cola";
+import CrearTicket from "./CrearTicket";
+import Escritorio from "./Escritorio";
 
 const { Sider, Content } = Layout;
 
 const RouterPage = () => {
   return (
-    <Layout style={{ height: "100vh" }}>
-      <Sider>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={[
-            {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "Ingresar",
-            },
-            {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "Cola",
-            },
-            {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "Crear Ticket",
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Content
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-            //background: colorBgContainer,
-            //borderRadius: borderRadiusLG,
-          }}
-        >
-          Content
-        </Content>
+    <Router>
+      <Layout style={{ height: "100vh" }}>
+        <Sider>
+          <div className="demo-logo-vertical" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              <Link to="/ingresar">Ingresar</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              <Link to="/cola">Cola de Tickets</Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Link to="/crear">Crear Ticket</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Routes>
+              <Route path="/ingresar" element={<Ingresar />} />
+              <Route path="/cola" element={<Cola />} />
+              <Route path="/crear" element={<CrearTicket />} />
+              <Route path="/escritorio" element={<Escritorio />} />
+            </Routes>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </Router>
   );
 };
 
