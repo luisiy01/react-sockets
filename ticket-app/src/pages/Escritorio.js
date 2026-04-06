f5c0661b2a4e331ed0e6bc8d596826460da36eff
Luis Nunez <luisnunez91@gmail.com>
2026-04-06 12:29:29 -0600

    pantalla escritorio

--- ticket-app/src/pages/Escritorio.js
+++ ticket-app/src/pages/Escritorio.js
@@ -1,7 +1,51 @@
-import React from "react";
+import { Row, Col, Typography, Button, Divider } from "antd";
+import { CloseCircleOutlined, RightOutlined } from "@ant-design/icons";
+
+const { Title, Text } = Typography;
 
 const Escritorio = () => {
-  return <div>Escritorio</div>;
+  const salir = () => {
+    console.log("salir");
+  };
+
+  const siguienteTicket = () => {
+    console.log("siguienteTicket");
+  };
+
+  return (
+    <>
+      <Row>
+        <Col span={20}>
+          <Title level={2}>Fernando</Title>
+          <Text>Usted esta trabjando en el escritorio: </Text>
+          <Text type="success">12</Text>
+        </Col>
+        <Col span={4} align="right">
+          <Button shape="round" type="danger" onClick={salir}>
+            <CloseCircleOutlined />
+            Salir
+          </Button>
+        </Col>
+      </Row>
+      <Divider />
+      <Row>
+        <Col>
+          <Text>Esta atendiendo el ticket numero: </Text>
+          <Text style={{ fontSize: 30 }} type="danger">
+            12
+          </Text>
+        </Col>
+      </Row>
+      <Row>
+        <Col offset={18} span={6} align="right">
+          <Button onClick={siguienteTicket} shape="round" type="primary">
+            <RightOutlined />
+            Siguiente
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Escritorio;
--- ticket-app/src/pages/Ingresar.js
+++ ticket-app/src/pages/Ingresar.js
@@ -1,9 +1,15 @@
-import { Button, Form, Input, InputNumber } from "antd";
+import { Button, Divider, Form, Input, InputNumber, Typography } from "antd";
 import { SaveOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+
+const { Title, Text } = Typography;
 
 const Ingresar = () => {
+  const navigate = useNavigate();
+
   const onFinish = (values) => {
     console.log("Success:", values);
+    navigate("/escritorio");
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -11,44 +17,49 @@ const Ingresar = () => {
   };
 
   return (
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 14 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Nombre del agente"
-        name="agente"
-        rules={[{ required: true, message: "Por favor ingrese su nombre" }]}
+    <>
+      <Title level={2}>Ingresar</Title>
+      <Text>Ingrese su nombre y número de escritorio</Text>
+      <Divider />
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 14 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Nombre del agente"
+          name="agente"
+          rules={[{ required: true, message: "Por favor ingrese su nombre" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Escritorio"
-        name="escritorio"
-        rules={[
-          {
-            required: true,
-            message: "Por favor ingrese el número de escritorio",
-          },
-        ]}
-      >
-        <InputNumber min={1} max={100} />
-      </Form.Item>
+        <Form.Item
+          label="Escritorio"
+          name="escritorio"
+          rules={[
+            {
+              required: true,
+              message: "Por favor ingrese el número de escritorio",
+            },
+          ]}
+        >
+          <InputNumber min={1} max={100} />
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 14 }}>
-        <Button type="primary" htmlType="submit" shape="round">
-          <SaveOutlined />
-          Ingresar
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item wrapperCol={{ offset: 8, span: 14 }}>
+          <Button type="primary" htmlType="submit" shape="round">
+            <SaveOutlined />
+            Ingresar
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
