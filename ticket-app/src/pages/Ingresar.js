faf16a8a8a172ecb77a0bf92456991367c338fac
Luis Nunez <luisnunez91@gmail.com>
2026-04-06 11:46:42 -0600

    pantalla ingresar

--- ticket-app/src/pages/Ingresar.js
+++ ticket-app/src/pages/Ingresar.js
@@ -1,7 +1,55 @@
-import React from "react";
+import { Button, Form, Input, InputNumber } from "antd";
+import { SaveOutlined } from "@ant-design/icons";
 
 const Ingresar = () => {
-  return <div>Ingresar</div>;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 14 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Nombre del agente"
+        name="agente"
+        rules={[{ required: true, message: "Por favor ingrese su nombre" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Escritorio"
+        name="escritorio"
+        rules={[
+          {
+            required: true,
+            message: "Por favor ingrese el número de escritorio",
+          },
+        ]}
+      >
+        <InputNumber min={1} max={100} />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 14 }}>
+        <Button type="primary" htmlType="submit" shape="round">
+          <SaveOutlined />
+          Ingresar
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default Ingresar;
--- ticket-app/src/pages/RouterPage.js
+++ ticket-app/src/pages/RouterPage.js
@@ -12,7 +12,7 @@ const RouterPage = () => {
   return (
     <Router>
       <Layout style={{ height: "100vh" }}>
-        <Sider>
+        <Sider collapsedWidth={0} breakpoint="md">
           <div className="demo-logo-vertical" />
           <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
             <Menu.Item key="1" icon={<UserOutlined />}>
