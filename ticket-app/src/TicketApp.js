e35b1aabdbcace1e2ed519efdf79d7334b7258c3
Luis Nunez <luisnunez91@gmail.com>
2026-04-07 10:41:55 -0600

    conectar react con el server socket

--- ticket-app/src/TicketApp.js
+++ ticket-app/src/TicketApp.js
@@ -1,14 +1,15 @@
 import React from "react";
 import RouterPage from "./pages/RouterPage";
 import { UiProvider } from "./context/UiContext";
+import { SocketProvider } from "./context/SocketContext";
 
 const TicketApp = () => {
   return (
-    <>
+    <SocketProvider>
       <UiProvider>
         <RouterPage />
       </UiProvider>
-    </>
+    </SocketProvider>
   );
 };
 
