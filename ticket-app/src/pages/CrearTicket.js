ab39ff8039de709c8b23c6780571165ca54a3ae1
Luis Nunez <luisnunez91@gmail.com>
2026-04-06 21:08:58 -0600

    crear ticket

--- ticket-app/src/pages/CrearTicket.js
+++ ticket-app/src/pages/CrearTicket.js
@@ -1,7 +1,39 @@
-import React from "react";
+import { Row, Col, Typography, Button, Divider } from "antd";
+import { CloseCircleOutlined, DownloadOutlined } from "@ant-design/icons";
+
+const { Title, Text } = Typography;
 
 const CrearTicket = () => {
-  return <div>CrearTicket</div>;
+  const nuevoTicket = () => {
+    console.log("nuevoTicket");
+  };
+  return (
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={3}>Presione el boton para un nuevo ticket</Title>
+          <Button
+            shape="round"
+            type="primary"
+            onClick={nuevoTicket}
+            size="large"
+          >
+            <DownloadOutlined />
+            Nuevo Ticket
+          </Button>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: "100px" }}>
+        <Col span={14} offset={6} align="center">
+          <Text level={2}>Su numero es: </Text>
+          <br />
+          <Text type="success" style={{ fontSize: 55 }} level={1}>
+            12
+          </Text>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default CrearTicket;
